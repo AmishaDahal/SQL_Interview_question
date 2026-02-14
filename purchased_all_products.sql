05d4536079b406dd0e10c72b56eab487980832b6
AmishaDahal <68108618+AmishaDahal@users.noreply.github.com>
2026-02-14 11:15:58 -0700

    Create purchased_all_products.sql

---
+++ purchased_all_products.sql
@@ -0,0 +1,11 @@
+/*67 List the customers who purchased all products in a specific category*/
+
+
+select customer_id
+from sales
+where category = 10 
+group by customer_id
+having count(distinct product_id) =
+(select count(distinct product_id)
+from products
+where category=10);
