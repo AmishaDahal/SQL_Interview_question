ed6b7bee417d16a8bbcbcf4c92c433a838327593
AmishaDahal <68108618+AmishaDahal@users.noreply.github.com>
2026-02-15 23:23:11 -0700

    Update percentage_difference_between_each_month_total.sql

--- percentage_difference_between_each_month_total.sql
+++ percentage_difference_between_each_month_total.sql
@@ -1,1 +1,15 @@
+/* 74. Find the percentage difference between each month’s total sales and the previous month’s total sales.*/
+
+
+WITH monthly_sales AS (
+ SELECT DATE_TRUNC('month', sale_date) AS
+month, SUM(amount) AS total_sales
+ FROM sales
+ GROUP BY month
+)
+
+SELECT month, total_sales,
+  (total_sales - LAG(total_sales) OVER (ORDER BY month)) * 100.0
+  / NULLIF(LAG(total_sales) OVER (ORDER BY month), 0) AS pct_change
+FROM monthly_sales;
 
